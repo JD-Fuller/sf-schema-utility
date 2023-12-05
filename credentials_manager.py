f6f21313c37b24118088c9f73a1a13a66a293f03
JD Fuller <42724384+JD-Fuller@users.noreply.github.com>
2023-12-05 02:56:21 -0700

    Update credentials_manager.py

--- credentials_manager.py
+++ credentials_manager.py
@@ -1,18 +1,17 @@
 import json
 import os
-from crypto_utils import CryptoUtils
+from crypto_utils import CryptoUtils  # Ensure this uses AES encryption
 
 def save_credentials(credentials):
     """
     Encrypt and save credentials to a file.
-
     Args:
         credentials (dict): A dictionary containing credentials.
     """
     crypto_utils = CryptoUtils()
     try:
         encrypted_credentials = crypto_utils.encrypt_data(json.dumps(credentials))
-        with open('.credentials.json', 'wb') as file:
+        with open('.credentials.json', 'w') as file:  # Note the change to 'w'
             file.write(encrypted_credentials)
     except Exception as e:
         print("Error saving credentials:", e)
@@ -20,13 +19,12 @@ def save_credentials(credentials):
 def get_credentials():
     """
     Retrieve and decrypt credentials from a file.
-
     Returns:
         dict: A dictionary of decrypted credentials.
     """
     crypto_utils = CryptoUtils()
     try:
-        with open('.credentials.json', 'rb') as file:
+        with open('.credentials.json', 'r') as file:  # Note the change to 'r'
             encrypted_credentials = file.read()
             decrypted_credentials = crypto_utils.decrypt_data(encrypted_credentials)
             return json.loads(decrypted_credentials) if decrypted_credentials else None
@@ -36,8 +34,12 @@ def get_credentials():
 
 # Example usage
 if __name__ == "__main__":
-    # Example credentials
-    creds = {"username": "user", "password": "pass"}
+    creds = {
+        "username": "jdfuller@resilient-bear-szma4m.com",
+        "password": "Thisistheway1",
+        "security_token": "HuNkFTchD9rJ4891R36zLP4Je",
+        "base_path": "https://login.salesforce.com"
+    }
     save_credentials(creds)
 
     retrieved_creds = get_credentials()
