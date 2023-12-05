e551425df7cbc2f488cb84ab28523d2f88f33b1e
JD Fuller <42724384+JD-Fuller@users.noreply.github.com>
2023-12-04 23:13:36 -0700

    Update sf_authentication.py

--- sf_authentication.py
+++ sf_authentication.py
@@ -6,6 +6,7 @@ and retrieve session information for further API interactions.
 
 from collections import namedtuple
 import requests
+from credentials_manager import get_credentials
 
 # Using namedtuple for configuration data if no additional methods are needed
 AuthConfig = namedtuple(
@@ -17,7 +18,8 @@ class SfAuth:
     """Handles Salesforce authentication."""
 
     def get_session_id_un_pw(self, config):
-        """Authenticate with username and password.
+        """
+        Authenticate with username and password.
 
         Args:
             config (AuthConfig): Configuration containing all necessary parameters.
@@ -25,13 +27,14 @@ class SfAuth:
         Returns:
             dict: The session ID or access token to be used for subsequent API calls.
         """
-        # Here you would implement the logic to authenticate using username and password.
-        # Since the logic is not provided, this is a placeholder for where it would go.
-        # Replace the following line with the actual implementation.
+        # Implement the logic to authenticate using username and password.
+        # This will likely involve sending a request to Salesforce's authentication endpoint.
+        # Replace the 'NotImplementedError' with actual implementation.
         raise NotImplementedError("The method get_session_id_un_pw is not implemented.")
 
     def get_session_id_conn_app(self, config):
-        """Authenticate using a connected app.
+        """
+        Authenticate using a connected app.
 
         Args:
             config (AuthConfig): Configuration containing all necessary parameters.
@@ -39,26 +42,26 @@ class SfAuth:
         Returns:
             dict: The session ID from Salesforce.
         """
-        password_with_token = f"{config.password}{config.security_token}"
-        payload = {
-            'grant_type': 'password',
-            'client_id': config.client_id,
-            'client_secret': config.client_secret,
-            'username': config.username,
-            'password': password_with_token
-        }
-        auth_url = f"{config.base_path}{self.get_auth_path()}"
-        response = requests.post(auth_url, data=payload, timeout=10)
-        response.raise_for_status()
-        return response.json()
+        try:
+            password_with_token = f"{config.password}{config.security_token}"
+            payload = {
+                'grant_type': 'password',
+                'client_id': config.client_id,
+                'client_secret': config.client_secret,
+                'username': config.username,
+                'password': password_with_token
+            }
+            auth_url = f"{config.base_path}{self.get_auth_path()}"
+            response = requests.post(auth_url, data=payload, timeout=10)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            print(f"Error in Salesforce Connected App Authentication: {e}")
+            return None
 
     @staticmethod
     def get_auth_path():
-        """Retrieve the path for the Salesforce OAuth service.
-
-        Returns:
-            str: The OAuth service path.
-        """
+        """Retrieve the path for the Salesforce OAuth service."""
         return '/services/oauth2/token'
 
 # Example usage should be implemented in a separate test file or environment.
