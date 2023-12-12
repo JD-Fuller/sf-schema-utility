6a1a2d796841d339d26eed8411f91bb849df2a69
JD Fuller <42724384+JD-Fuller@users.noreply.github.com>
2023-12-12 02:00:10 -0700

    updated per zap recommendations

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 # app.py
 
 from flask import Flask, request, render_template
+from sf_authentication import authenticate_using_username_password
 from sf_describe_logic import get_available_objects, describe_object
-# Other necessary imports...
 
 app = Flask(__name__)
+
+@app.after_request
+def apply_security_headers(response):
+    # Content Security Policy
+    csp_policy = "default-src 'self'; script-src 'self'; object-src 'none';"
+    response.headers['Content-Security-Policy'] = csp_policy
+
+    # X-Frame-Options
+    response.headers['X-Frame-Options'] = 'SAMEORIGIN'
+    
+    return response
 
 @app.route('/')
 def index():
@@ -15,9 +26,10 @@ def describe():
     if request.method == 'POST':
         object_name = request.form.get('object_name')
         environment = request.form.get('environment')
-        sf = authenticate_to_salesforce(environment)  # Ensure this function is properly imported
-        if sf:
-            result = describe_object(object_name, sf)
+        token_data = authenticate_using_username_password(environment)
+        
+        if 'access_token' in token_data:
+            result = describe_object(object_name, token_data)
             return render_template('describe_result.html', result=result)
         else:
             return render_template('describe_error.html', error="Salesforce authentication failed.")
--- sf_describe_logic.py
+++ sf_describe_logic.py
@@ -1,18 +1,22 @@
 # sf_describe_logic.py
 
 import logging
-from sf_authentication import authenticate_to_salesforce
+from simple_salesforce import Salesforce
 
-def get_available_objects(sf_instance):
+def get_available_objects(token_data):
     try:
-        return sf_instance.describe()
+        sf = Salesforce(instance_url=token_data['instance_url'],
+                        session_id=token_data['access_token'])
+        return sf.describe()
     except Exception as e:
         logging.error(f"Error retrieving object list: {e}")
         return None
 
-def describe_object(sObjectName, sf_instance):
+def describe_object(sObjectName, token_data):
     try:
-        return sf_instance.sobjects[sObjectName].describe()
+        sf = Salesforce(instance_url=token_data['instance_url'],
+                        session_id=token_data['access_token'])
+        return sf.sobjects[sObjectName].describe()
     except Exception as e:
         logging.error(f"Error describing {sObjectName}: {e}")
         return None
